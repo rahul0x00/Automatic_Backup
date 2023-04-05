aee14ecf2a2c370add636d125d0dfe1d1e33e050
rahul0x00 <rahul0x00@gmail.com>
2023-04-05 22:19:05 +0530

    added logging

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Automatic Backup 
 
-### This scripts creates a compressed backup archive of the files in the specified directory,and copies it to the destination directory
+### This scripts creates a compressed backup archive of the files in the specified directory,and copies it to the destination directory 
+
+### Logs the success or failure of the backup process to a log file 
 
 
--- autoBackup.sh
+++ autoBackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prompt to get Backup Path directory
-read -p "Enter the path of Backup Directory(e.g. /path/of/backup/dir): " BAK_DIR
+read -p "Enter the path of Backup Directory(e.g. /path/of/backup/dir): " SRC_DIR
 
 # Prompt to get Destination Path directory
 read -p "Enter the path to Destination Directory(e.g. /path/to/destination/dir): " DEST_DIR
@@ -9,11 +9,17 @@ read -p "Enter the path to Destination Directory(e.g. /path/to/destination/dir):
 # Create a timestamp for the backup filename
 TIMESTAMP=$(date +%Y-%m-%d_%H%M%S)
 
+# Create Backup Directory if it doesn't exist
+if [ ! -d $DEST_DIR ];
+then
+	mkdir -p "$DEST_DIR"
+fi
+
 #Log file
 LOG_FILE=$DEST_DIR/LOG_FILE
 
 # Create the backup archive
-tar -czf $DEST_DIR/backup_$TIMESTAMP.tar.gz -C $BAK_DIR .
+tar -czf $DEST_DIR/backup_$TIMESTAMP.tar.gz -C $SRC_DIR .
 
 # Copy the backup archive to the destination directory
 cp $DEST_DIR/backup_$TIMESTAMP.tar.gz $DEST_DIR/backup_latest.tar.gz 
