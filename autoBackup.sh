100e40e66fa0762547b54e24146f35f912a06036
rahul0x00 <rahul0x00@gmail.com>
2023-04-05 10:18:32 +0530

    Added Log File in the Destination Directory with each backup

--- autoBackup.sh
+++ autoBackup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Prompt to get Backup Path directory
-read -p "Enter the path of Backup Directory: " BAK_DIR
+read -p "Enter the path of Backup Directory(e.g. /path/of/backup/dir: " BAK_DIR
 
 # Prompt to get Destination Path directory
-read -p "Enter the path to Destination Directory:(e.g. /path/to/destination/dir " DEST_DIR
+read -p "Enter the path to Destination Directory(e.g. /path/to/destination/dir: " DEST_DIR
 
 # Create a timestamp for the backup filename
 TIMESTAMP=$(date +%Y-%m-%d_%H%M%S)
+
+#Logging status
+LOG_FILE=$DEST_DIR/LOG_FILE
 
 # Create the backup archive
 tar -czf $DEST_DIR/backup_$TIMESTAMP.tar.gz -C $BAK_DIR .
@@ -15,4 +18,9 @@ tar -czf $DEST_DIR/backup_$TIMESTAMP.tar.gz -C $BAK_DIR .
 # Copy the backup archive to the destination directory
 cp $DEST_DIR/backup_$TIMESTAMP.tar.gz $DEST_DIR/backup_latest.tar.gz 
 
-echo "Done"
+# Log status of operation
+echo "$(date "+%Y-%m-%d %H:%M:%S"): Backup successful!" >> $LOG_FILE
+
+# Print the last line of log file
+tail -1 $DEST_DIR/LOG_FILE
+
